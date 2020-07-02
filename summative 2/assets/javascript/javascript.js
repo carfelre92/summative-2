b635454ea417614c54ee208b4b9f14be5dae9a1c
Jin Kim <wlsgns92@hotmail.com>
2020-07-02 16:24:32 +1200

    simple nav

--- summative 2/assets/javascript/javascript.js
+++ summative 2/assets/javascript/javascript.js
@@ -1,5 +1,18 @@
+//JS
+function removeBottomAddCurrent(a) {
+    a.removeClass('bottom');
+    a.addClass('current');
+}
+
+function addBottomRemoveCurrent(a) {
+    a.addClass('bottom');
+    a.removeClass('current');
+}
+
+//Jquery
 $(function () {
 
+    //all animations
     let mainWelcome = anime.timeline({
         easing: 'easeInOutQuad',
         autoplay: false,
@@ -16,29 +29,62 @@ $(function () {
         translateY: ['-8%', '100%'],
     })
 
+    mainWelcome.add({
+        targets: '.main-welcome-container',
+        zIndex: [0],
+    })
+
+    ////////////end of animation
+
+    //main to login
     $('.main-button').on('click', function () {
         mainWelcome.play();
         setTimeout(function () {
-            $('.login-container').addClass('current');
+            removeBottomAddCurrent($('.login-container'))
         }, 900)
     });
 
+    //login to phone-auth
     $('.continue-to-phone-auth').on('click', function () {
-        let thisThing =$(this).parent().parent().parent();
-        let phoneLoginContainer=$('.phone-login-container')
-        thisThing.css('opacity',0)
-        thisThing.removeClass('current')
-        thisThing.addClass('bottom-side')
-        phoneLoginContainer.css('opacity',1)
-        phoneLoginContainer.addClass('current')
+        let toPhoneInput = $(this).prev().prev().children().next();
+        let loginContainer = $(this).parent().parent().parent();
+        let phoneAuthContainer = $(this).parent().parent().parent().next();
+        let phoneRegex = /^[+]*[(]{0,1}[0-9]{1,4}[)]{0,1}[-\s\./0-9]*$/
+        let pResult = phoneRegex.test(toPhoneInput.val());
+
+        if (pResult == false) {
+            console.log(pResult);
+            $(this).prev().css('display', 'block');
+        } else {
+            console.log(pResult);
+            $(this).prev().css('display', 'none');
+            removeBottomAddCurrent(phoneAuthContainer)
+            addBottomRemoveCurrent(loginContainer)
+        }
     })
-    
-    $('.back-to-login').children().on('click', function(){
-        $('.login-container').removeClass('bottom-side')
-        $('.login-container').css('opacity',1)
-        $('.phone-login-container').css('opacity',0)
-        $('.phone-login-container').removeClass('current')
-        $('.phone-login-container').addClass('bottom-side')
+
+    //phone-auto to login
+    $('.back-to-login-from-phone-auth').children().on('click', function () {
+        let phoneLoginContainer = $(this).parent().parent().parent().parent();
+        let loginContainer = phoneLoginContainer.prev();
+        removeBottomAddCurrent(loginContainer)
+        addBottomRemoveCurrent(phoneLoginContainer)
+    })
+
+    //login to email-login
+    $('.sign-in-alternative').children().on('click', function () {
+        let loginContainer = $(this).parent().parent().parent().parent();
+        let loginEmailContainer = loginContainer.next().next();
+        removeBottomAddCurrent(loginEmailContainer);
+        addBottomRemoveCurrent(loginContainer);
+    })
+
+    //email-login to login
+    $('.go-to-login').children().on('click', function () {
+        let loginEmailContainer = $(this).parent().parent().parent().parent();
+        let loginContainer = loginEmailContainer.prev().prev();
+        removeBottomAddCurrent(loginContainer);
+        addBottomRemoveCurrent(loginEmailContainer);
     })
 })
 
